eba909e99f8a7a34cf512d39d30e07cf7165e238
Mihai Costea <mihai.andrei.costea@gmail.com>
2012-12-10 19:09:10 +0200

    These files are already in the source tree

--- config/common_full.mk
+++ config/common_full.mk
@@ -1,12 +1,6 @@
 PRODUCT_BRAND ?= cerebrum
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-
-PRODUCT_COPY_FILES += \
-    vendor/cerebrum/prebuilt/common/bin/sysinit:system/bin/sysinit \
-    vendor/cerebrum/prebuilt/common/etc/mkshrc:system/etc/mkshrc \
-    vendor/cerebrum/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
-    vendor/cerebrum/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd \
     
 $(call inherit-product, vendor/cerebrum/config/supersu.mk)
 
