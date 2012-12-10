dcd49e257553241ed58518b42e395e24e8a17d00
Mihai Costea <mihai.andrei.costea@gmail.com>
2012-12-10 12:02:27 +0200

    Revert "Use BUILD_PREBUILT instead of PRODUCT_COPY_FILES"
    This reverts commit 164d276e3b7c8bf19bb1dd47e7f4eab3a2c3d722.

--- config/supersu.mk
+++ config/supersu.mk
@@ -1,5 +1,3 @@
 PRODUCT_COPY_FILES += \
-	vendor/cerebrum/prebuilt/common/xbin/su:system/xbin/su
-
-BUILD_PREBUILT += \
+	vendor/cerebrum/prebuilt/common/xbin/su:system/xbin/su \
 	vendor/cerebrum/prebuilt/common/app/Superuser.apk:system/app/Superuser.apk
