164d276e3b7c8bf19bb1dd47e7f4eab3a2c3d722
Mihai Costea <mihai.andrei.costea@gmail.com>
2012-12-10 11:55:31 +0200

    Use BUILD_PREBUILT instead of PRODUCT_COPY_FILES

--- config/supersu.mk
+++ config/supersu.mk
@@ -1,3 +1,5 @@
 PRODUCT_COPY_FILES += \
-	vendor/cerebrum/prebuilt/common/xbin/su:system/xbin/su \
+	vendor/cerebrum/prebuilt/common/xbin/su:system/xbin/su
+
+BUILD_PREBUILT += \
 	vendor/cerebrum/prebuilt/common/app/Superuser.apk:system/app/Superuser.apk
