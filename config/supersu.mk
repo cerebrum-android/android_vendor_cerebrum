72e673a34dc79dd959577b71eb00e692f06dcaf5
Mihai Costea <mihai.andrei.costea@gmail.com>
2012-12-10 19:22:29 +0200

    Use cerebrum as owner

--- config/supersu.mk
+++ config/supersu.mk
@@ -1,3 +1,3 @@
 PRODUCT_COPY_FILES += \
-	vendor/cerebrum/prebuilt/common/xbin/su:system/xbin/su:SuperSU \
-	vendor/cerebrum/prebuilt/common/app/Superuser.apk:system/app/Superuser.apk:SuperSU
+	vendor/cerebrum/prebuilt/common/xbin/su:system/xbin/su:cerebrum \
+	vendor/cerebrum/prebuilt/common/app/Superuser.apk:system/app/Superuser.apk:cerebrum
