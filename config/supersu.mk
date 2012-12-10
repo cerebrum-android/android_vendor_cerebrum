9461e4d60528e7caed1928e4ab7856015c2a6650
Mihai Costea <mihai.andrei.costea@gmail.com>
2012-12-10 19:12:24 +0200

    Add owner to PRODUCT_COPY_FILES

--- config/supersu.mk
+++ config/supersu.mk
@@ -1,3 +1,3 @@
 PRODUCT_COPY_FILES += \
-	vendor/cerebrum/prebuilt/common/xbin/su:system/xbin/su \
-	vendor/cerebrum/prebuilt/common/app/Superuser.apk:system/app/Superuser.apk
+	vendor/cerebrum/prebuilt/common/xbin/su:system/xbin/su:SuperSU \
+	vendor/cerebrum/prebuilt/common/app/Superuser.apk:system/app/Superuser.apk:SuperSU
